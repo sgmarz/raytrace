681aaff8c9cad26da4d388c170984ddfd6052f59
Stephen Marz <stephen.marz@utk.edu>
2020-12-08 12:45:54 -0500

    Updates

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,5 @@
+
+use std::ops::{Index, IndexMut};
 pub struct Color {
     red: f64,
     green: f64,
@@ -30,6 +32,29 @@ impl Default for Color {
             red: 0.0,
             green: 0.0,
             blue: 0.0
+        }
+    }
+}
+
+impl Index<usize> for Color {
+    type Output = f64;
+    fn index(&self, idx: usize) -> &Self::Output {
+        match idx {
+            0 => &self.red,
+            1 => &self.green,
+            2 => &self.blue,
+            _ => panic!("Invalid color index {}", idx)
+        }
+    }
+}
+
+impl IndexMut<usize> for Color {
+    fn index_mut(&mut self, idx: usize) -> &mut Self::Output {
+        match idx {
+            0 => &mut self.red,
+            1 => &mut self.green,
+            2 => &mut self.blue,
+            _ => panic!("Invalid color index {}", idx)
         }
     }
 }
--- src/pixel.rs
+++ src/pixel.rs
@@ -1,4 +1,6 @@
 
+use std::ops::{Index, IndexMut};
+
 pub struct Pixel {
     red: u8,
     green: u8,
@@ -37,6 +39,29 @@ impl Default for Pixel {
             red: 0,
             green: 0,
             blue: 0
+        }
+    }
+}
+
+impl Index<usize> for Pixel {
+    type Output = u8;
+    fn index(&self, idx: usize) -> &Self::Output {
+        match idx {
+            0 => &self.red,
+            1 => &self.green,
+            2 => &self.blue,
+            _ => panic!("Invalid pixel {}", idx)
+        }
+    }
+}
+
+impl IndexMut<usize> for Pixel {
+    fn index_mut(&mut self, idx: usize) -> &mut Self::Output {
+        match idx {
+            0 => &mut self.red,
+            1 => &mut self.green,
+            2 => &mut self.blue,
+            _ => panic!("Invalid pixel {}", idx)
         }
     }
 }
